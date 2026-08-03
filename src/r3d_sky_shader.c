eef497d0ff29daad15301f52d9c1ff527eb95933
Bigfoot71 <victor.lejuez@protonmail.ch>
2026-08-03 21:44:50 +0200

    style update - r3d_sky_shader.c

--- src/r3d_sky_shader.c
+++ src/r3d_sky_shader.c
@@ -28,7 +28,8 @@ static bool compile_shader(R3D_SkyShader* shader);
 R3D_SkyShader* R3D_LoadSkyShader(const char* filePath)
 {
     char* code = LoadFileText(filePath);
-    if (code == NULL) {
+    if (code == NULL)
+    {
         R3D_TRACELOG(LOG_ERROR, "Failed to load sky shader; Unable to load shader file");
         return NULL;
     }
@@ -42,18 +43,21 @@ R3D_SkyShader* R3D_LoadSkyShader(const char* filePath)
 R3D_SkyShader* R3D_LoadSkyShaderFromMemory(const char* code)
 {
     size_t userCodeLen = strlen(code);
-    if (userCodeLen > R3D_MAX_SHADER_CODE_LENGTH) {
+    if (userCodeLen > R3D_MAX_SHADER_CODE_LENGTH)
+    {
         R3D_TRACELOG(LOG_ERROR, "Failed to load sky shader; User code too long");
         return NULL;
     }
 
-    if (!strstr(code, "void fragment()")) {
+    if (!strstr(code, "void fragment()"))
+    {
         R3D_TRACELOG(LOG_WARNING, "Failed to load sky shader; Missing fragment() entry point");
         return NULL;
     }
 
     R3D_SkyShader* shader = r3d_shader_custom_alloc();
-    if (!shader) {
+    if (!shader)
+    {
         R3D_TRACELOG(LOG_ERROR, "Bad alloc during sky shader loading");
         return NULL;
     }
@@ -74,7 +78,8 @@ R3D_SkyShader* R3D_LoadSkyShaderFromMemory(const char* code)
         if (!*ptr) break;
 
         // Parse uniform declarations
-        if (r3d_rshade_match_keyword(ptr, "uniform", 7)) {
+        if (r3d_rshade_match_keyword(ptr, "uniform", 7))
+        {
             ptr += 7;
             r3d_rshade_parse_uniform(&ptr,
                 shader->data.samplers,
@@ -90,9 +95,11 @@ R3D_SkyShader* R3D_LoadSkyShaderFromMemory(const char* code)
 
         // Parse fragment() function
         r3d_rshade_parsed_function_t* func = r3d_rshade_check_shader_entry(ptr, NULL, &fragmentFunc);
-        if (func) {
+        if (func)
+        {
             r3d_rshade_skip_to_brace(&ptr);
-            if (*ptr == '{') {
+            if (*ptr == '{')
+            {
                 func->bodyStart = ptr;
                 r3d_rshade_skip_to_matching_brace(&ptr);
                 func->bodyEnd = ptr;
@@ -119,7 +126,8 @@ R3D_SkyShader* R3D_LoadSkyShaderFromMemory(const char* code)
     r3d_rshade_copy_global_code(&writer, code, false, NULL, &fragmentFunc);
     r3d_rshade_write_shader_function(&writer, "fragment", &fragmentFunc);
 
-    if (writer.overflow) {
+    if (writer.overflow)
+    {
         R3D_TRACELOG(LOG_ERROR, "Failed to load sky shader; Transformed code too long");
         MemFree(shader);
         return NULL;
@@ -129,7 +137,8 @@ R3D_SkyShader* R3D_LoadSkyShaderFromMemory(const char* code)
 
     /* --- PHASE 3: Compile shader --- */
 
-    if (!compile_shader(shader)) {
+    if (!compile_shader(shader))
+    {
         R3D_UnloadSkyShader(shader);
         return NULL;
     }
@@ -148,7 +157,8 @@ R3D_SkyShader* R3D_LoadSkyShaderFromMemory(const char* code)
 R3D_SkyShader* R3D_LoadSkyShaderAlias(R3D_SkyShader* shader)
 {
     R3D_SkyShader* alias = r3d_shader_custom_clone(shader);
-    if (!alias) {
+    if (!alias)
+    {
         R3D_TRACELOG(LOG_ERROR, "Bad alloc during sky shader alias loading");
         return NULL;
     }
@@ -162,24 +172,28 @@ void R3D_UnloadSkyShader(R3D_SkyShader* shader)
 
 void R3D_SetSkyShaderUniform(R3D_SkyShader* shader, const char* name, const void* value)
 {
-    if (!shader) {
+    if (!shader)
+    {
         R3D_TRACELOG(LOG_WARNING, "Cannot set uniform '%s' on NULL sky shader", name);
         return;
     }
 
-    if (!r3d_shader_custom_set_uniform(shader, name, value)) {
+    if (!r3d_shader_custom_set_uniform(shader, name, value))
+    {
         R3D_TRACELOG(LOG_WARNING, "Failed to set custom uniform '%s'", name);
     }
 }
 
 void R3D_SetSkyShaderSampler(R3D_SkyShader* shader, const char* name, Texture texture)
 {
-    if (!shader) {
+    if (!shader)
+    {
         R3D_TRACELOG(LOG_WARNING, "Cannot set sampler '%s' on NULL sky shader", name);
         return;
     }
 
-    if (!r3d_shader_custom_set_sampler(shader, name, texture)) {
+    if (!r3d_shader_custom_set_sampler(shader, name, texture))
+    {
         R3D_TRACELOG(LOG_WARNING, "Failed to set custom sampler '%s'", name);
     }
 }
@@ -191,7 +205,8 @@ void R3D_SetSkyShaderSampler(R3D_SkyShader* shader, const char* name, Texture te
 bool compile_shader(R3D_SkyShader* shader)
 {
     bool ok = R3D_MOD_SHADER_LOADER.prepare.cubemapCustomSky(shader);
-    if (!ok) {
+    if (!ok)
+    {
         R3D_TRACELOG(LOG_ERROR, "Failed to compile sky shader");
     }
     return ok;
