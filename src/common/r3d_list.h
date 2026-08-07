1818516b1696b1dc7cf80c21a6e0d84c6f088191
Bigfoot71 <victor.lejuez@protonmail.ch>
2026-08-07 21:58:56 +0200

    add list setter

--- src/common/r3d_list.h
+++ src/common/r3d_list.h
@@ -6,6 +6,9 @@
  * For conditions of distribution and use, see accompanying LICENSE file.
  */
 
+#ifndef R3D_COMMON_LIST_H
+#define R3D_COMMON_LIST_H
+
 #include <raylib.h>
 #include <stddef.h>
 
@@ -36,6 +39,7 @@ typedef struct {
 #define R3D_LIST_EMPTY(list)                    r3d_list_empty((list))
 #define R3D_LIST_CLEAR(list)                    r3d_list_clear((list))
 #define R3D_LIST_GET(list, type, index)         (((type*)((list)->elements))[index])
+#define R3D_LIST_SET(list, type, index, value)  (((type*)((list)->elements))[index] = (type)(value))
 #define R3D_LIST_GET_INDEX(list, elemPtr)       (((char*)(elemPtr) - (char*)((list)->elements)) / (list)->elemSize)
 
 #define R3D_LIST_FOR_EACH(list, type, elem) \
@@ -56,3 +60,5 @@ void r3d_list_unordered_remove(r3d_list_t* list, size_t index);
 size_t r3d_list_length(r3d_list_t* list);
 bool r3d_list_empty(r3d_list_t* list);
 void r3d_list_clear(r3d_list_t* list);
+
+#endif // R3D_COMMON_LIST_H
