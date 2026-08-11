9da7b40928da09281d5afb01e4ae112581d0834d
Bigfoot71 <victor.lejuez@protonmail.ch>
2026-08-11 14:23:38 +0200

    remove lumen <-> energy conversion functions

--- include/r3d/r3d_lighting.h
+++ include/r3d/r3d_lighting.h
@@ -128,38 +128,11 @@ R3DAPI bool R3D_IsShadowMapValid(R3D_ShadowMap shadowMap);
  * This function is only relevant for spotlights and omni-lights.
  * 
  * @note This function should be called while using the default 3D rendering mode of raylib, 
- *       not with R3D's rendering mode. It uses raylib's 3D drawing functions to render the light's shape.
+ *       not with r3d's rendering mode. It uses raylib's 3D drawing functions to render the light's shape.
  *
  * @param light The light to visualize (see R3D_Light).
  */
 R3DAPI void R3D_DrawLightDebug(R3D_Light light);
-
-// ----------------------------------------
-// LIGHTING: Math Helper Functions
-// ----------------------------------------
-
-/**
- * @brief Converts a luminous flux to an energy factor.
- *
- * Computes the illuminance in lux at the given reference distance from an isotropic
- * point source: `energy = lumens / (4 * pi * distance * distance)`
- *
- * @param lumens The luminous flux in lumens.
- * @param referenceDistance The reference distance in scene units (1 unit = 1 meter).
- * @return The corresponding energy factor.
- */
-R3DAPI float R3D_LumensToEnergy(float lumens, float referenceDistance);
-
-/**
- * @brief Converts an energy factor back to a luminous flux.
- *
- * Inverse of @ref R3D_LumensToEnergy: `lumens = energy * 4 * pi * distance * distance`
- *
- * @param energy The energy factor.
- * @param referenceDistance The reference distance in scene units (1 unit = 1 meter).
- * @return The corresponding luminous flux in lumens.
- */
-R3DAPI float R3D_EnergyToLumens(float energy, float referenceDistance);
 
 #ifdef __cplusplus
 } // extern "C"
